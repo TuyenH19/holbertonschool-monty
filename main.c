7fdf1be16608116a0e47366f51d703aa72babc54
mkl-74 <mickael1008@gmail.com>
2024-01-03 12:01:21 +0000

    get, main, monty_V1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+#define STACK_SIZE 100
+
+
+
+int main()
+{
+
+
+
+
+
+    
+}
--- monty.h
+++ monty.h
@@ -2,14 +2,6 @@
 #define MONTY_H
 
 /**
-
-
-
-
-
-*/
-
-/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -37,6 +29,30 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+
+#define STACK_SIZE 100
+
+typedef struct {
+    int stack[STACK_SIZE];
+    int top;
+} Stack;
+
+void push(Stack *stack, int value, int line_number) {
+    if (stack->top == STACK_SIZE - 1) {
+        fprintf(stderr, "L%d: Stack overflow\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack->top++;
+    stack->stack[stack->top] = value;
+}
+
+void pall(Stack *stack) {
+    int i;
+    for (i = stack->top; i >= 0; i--) {
+        printf("%d\n", stack->stack[i]);
+    }
+}
 
 
 #endif
