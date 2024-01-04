a819fe1fc71bc849192f5185acaa439b3c9044e4
TuyenH19 <thtuyen199@gmail.com>
2024-01-04 07:35:48 +0000

    main file

---
+++ main.c
@@ -0,0 +1,19 @@
+#include"monty.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: Always 0
+*/
+int main(int argc, char **argv)
+{
+    (void) argv;
+
+    if (argc != 2)
+    {
+        dprintf(STDERR_FILENO, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    handle_file(argv[1]);
+    return (0);
+}
