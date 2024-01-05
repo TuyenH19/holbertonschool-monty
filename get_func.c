48078a3a86403f0b091b99787518984dbe28c26e
TuyenH19 <thtuyen199@gmail.com>
2024-01-05 06:20:33 +0000

    merge split_line to get_func

--- get_func.c
+++ get_func.c
@@ -1,39 +1,36 @@
 #include"monty.h"
 /**
  * get_func - pointer to the appropriate function
+ * @line: the line input to get function
+ * @stack: pointer to top of the stack
+ * @line_number: current line number in file
+ *
  * Return: Nothing
 */
-void (*get_func(void))(stack_t **, unsigned int)
+void get_func(char *line, stack_t **stack, unsigned int line_number)
 {
-	int i = 0, j;
-	int valid;
-	int length;
+	char *tokens = strtok(line, " \t\n");
+	char *tok = strtok(NULL, " \t\n");
 
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"nop", nop},
-		{NULL, NULL}
-	};
-	while (instructions[i].opcode)
+	if (!tokens)
+		return;
+	if (strcmp(tokens, "push") == 0)
+		push(stack, line_number, tok);
+	else if (strcmp(tokens, "pall") == 0)
+		pall(stack, line_number);
+	else if (strcmp(tokens, "pint") == 0)
+		pint(stack, line_number);
+	else if (strcmp(tokens, "pop") == 0)
+		pop(stack, line_number);
+	else if (strcmp(tokens, "swap") == 0)
+		swap(stack, line_number);
+	else if (strcmp(tokens, "add") == 0)
+		add(stack, line_number);
+	else if (strcmp(tokens, "nop") == 0)
+		nop(stack, line_number);
+	else
 	{
-		j = 0;
-		valid = 1;
-		length = strlen(instructions[i].opcode);
-		while (j < length)
-		{
-			if (instructions[i].opcode[j] != line[j])
-			{
-				valid = 0;
-				break;
-			}
-			j++;
-		}
-		if (valid == 1 && (line[j] == '\n' || line[j] == ' ' || line[j] == '\0'))
-			break;
-		i++;
+		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", line_number, tokens);
+		exit(EXIT_FAILURE);
 	}
-	return (instructions[i].f);
 }
