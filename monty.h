1675cb8e2da20fe775e854862aada2b8cbcff0a9
TuyenH19 <thtuyen199@gmail.com>
2024-01-05 06:54:22 +0000

    modify function push

--- monty.h
+++ monty.h
@@ -38,11 +38,10 @@ typedef struct instruction_s
 } instruction_t;
 
 void handle_file(char *filename);
-void (*get_func(void))(stack_t **, unsigned int);
-char **split_line(char *string);
-
+void get_func(char *line, stack_t **stack, unsigned int line_number);
+int is_integer(const char *str);
 void free_stack(stack_t *stack);
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
