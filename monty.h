df7d7e7816f2113411cd66221feddf032dbce548
TuyenH19 <thtuyen199@gmail.com>
2024-01-05 07:24:40 +0000

    add library for special types

--- monty.h
+++ monty.h
@@ -1,11 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
-#define TOK_DELIM " "
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
