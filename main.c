ee013ca2de7251b9a2fb6a775f194ee0e834403f
TuyenH19 <thtuyen199@gmail.com>
2024-01-05 08:02:38 +0000

    modify message error of file extension

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	filename = argv[1];
 	if (strcmp(get_file_extension(filename), "m") != 0)
 	{
-		dprintf(STDERR_FILENO, "File %s must have .m extension\n", filename);
+		dprintf(STDERR_FILENO, "%s containing monty instructions with an error\n", filename);
 		exit(EXIT_FAILURE);
 	}
 	handle_file(filename);
